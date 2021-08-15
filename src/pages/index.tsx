d338b55bc60d01c8fad23c11a4bcdf9a351643d9
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-14 21:38:12 -0300

    Finalizando Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,11 +8,16 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <main>
-        <section>
-
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
         </section>
-        <img src="/images/avatar.svg" alt="Girl Coding" />
+        <img src="/images/avatar.svg" alt="ig.news"/>
       </main>
     </>
   )

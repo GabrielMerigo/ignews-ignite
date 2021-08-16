4fc4536370b6c81c0bdda0c6aa59281eff6ce2ef
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-16 16:17:25 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
+import { useEffect } from 'react';
 
 interface HomeProps {
   product: {
@@ -33,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JOYB2Fq1V9nElokkIjLaemK')
 
   const product = {
@@ -47,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }

f604d8aa0a675b3e76dcfee47f49ab64a31b2ced
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-20 21:15:51 -0300

    Evitando duplique usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,17 +11,28 @@ export default NextAuth({
       scope: 'read:user' // Pegar os dados mais básicos do github usado pra autenticar
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ), q.Create(
+              q.Collection('users'),
+              { data: {email } }
+            ), q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 

d24f83f9c9e6d1afbcddc52f586d452764443f56
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-08-23 23:12:17 -0300

    adicioando js do stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,10 +9,20 @@ interface SubscribeButtonProps {
 export function SubscribeButton(props: SubscribeButtonProps){
   const [session] = useSession()
 
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      
+    }catch{
+
     }
 
   }
